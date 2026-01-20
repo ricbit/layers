21db3d5d5cdc7cdc59eaafdb99d5855890ab6d93
Ricardo Bittencourt <bluepenguin@gmail.com>
2026-01-20 19:03:43 -0300

    Refactored probO for legibility

--- 2026-3/probO.py
+++ 2026-3/probO.py
@@ -2,26 +2,19 @@ import cv2
 import pytesseract
 
 img = cv2.imread("formiga.png")
-data = [[0] * 100 for _ in range(100)]
-for j in range(100):
-  for i in range(100):
-    data[j][i] = int(img[j][i][0] == 255)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-sx, sy = 36, 50
-pos = sy * 1j + sx
-fdir = 1j
+startx, starty = 36, 50
+pos = starty * 1j + startx
+direction = 1j
 for _ in range(10000):
-  pos -= fdir
-  sy, sx = int(pos.imag), int(pos.real)
-  if data[sy][sx] == 1:
-    data[sy][sx] = 0
-    fdir *= 1j
+  pos -= direction
+  y, x = int(pos.imag), int(pos.real)
+  if img[y][x] > 127:
+    img[y][x] = 0
+    direction *= 1j
   else:
-    data[sy][sx] = 1
-    fdir *= -1j
-
-for j in range(100):
-  for i in range(100):
-    img[j][i] = ((255, 255, 255) if (data[j][i] > 0) else (0, 0, 0))
+    img[y][x] = 255
+    direction *= -1j
 
 print(pytesseract.image_to_string(img).upper())
